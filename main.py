12ca14850e24a14b92565d44a0873a5a1e6b1ef9
Arafat <betopia@Foysals-Mac.local>
2025-12-15 16:40:28 +0600

    MOD: Minor adjustment

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """FastAPI Application Entry Point"""
 from src.app import create_app
+import uvicorn
 
 # Create the FastAPI application instance
 app = create_app()
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
